9f3ac50ef211a2f2a989b1044ecc32db65b4405e
Benjamín Cepeda <bcepeda_98@yahoo.com>
2022-04-25 00:26:58 -0500

    Agregar archivos de proyecto.

---
+++ regemp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace regemp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private  async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string nombreUsuario = "";
+            string mensaje = validarIngreso(txtUser.Text, txtPassword.Text);
+            if (mensaje!="") {
+                await DisplayAlert("Error de validación", mensaje, "Continuar");
+            }
+            else
+            {
+                nombreUsuario = txtUser.Text;
+                await Navigation.PushAsync(new Inicio(nombreUsuario));
+            }
+        }
+        private string validarIngreso(string usuario, string clave)
+        {
+            string mensaje = "";
+            if (usuario != "estudiante2022")
+            {
+                mensaje = "Usuario incorrecto !";
+            } else if (clave != "uisrael2022")
+            {
+                mensaje = "Clave incorrecta !";
+            }
+            return mensaje;
+        }
+    }
+}
