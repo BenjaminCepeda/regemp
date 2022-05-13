d5644ae782fa92519abb33c7dcdbdee18fbda52e
Benjamín Cepeda <bcepeda_98@yahoo.com>
2022-05-13 12:34:46 -0500

    Fix mensajes separar constantes Api de Client

--- regemp/ApiClient/ApiConstants.cs
+++ regemp/ApiClient/ApiConstants.cs
@@ -5,7 +5,9 @@ using System.Text;
 namespace regemp.ApiClient
 {
     static class ApiConstants
-    {
+    {        
+        // NO FUNCIONA CON 127.0.0.1
+        // DIRECCION DEL SERVICIO PUBLICADO https://stormy-badlands-17405.herokuapp.com
         public const string API_URL = "https://stormy-badlands-17405.herokuapp.com/";
     }
 }
